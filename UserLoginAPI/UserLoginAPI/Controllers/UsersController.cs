0a2e9fbbb41c95e8059e08bf8985e519b366e527
Bhavin Patel <mailmebhav@gmail.com>
2018-08-30 13:07:42 +0530

    Added Basic UserLoginAPI with basic CRUD operations and password hashing

---
+++ UserLoginAPI/UserLoginAPI/Controllers/UsersController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserLoginAPI.Models;
+
+namespace UserLoginAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserLoginAPIContext _context;
+
+        private string Salt = "gj+oAMieIg+2B/eoxA31+w==";
+        private byte[] Saltbyte;
+
+        public UsersController(UserLoginAPIContext context)
+        {
+            _context = context;
+            Saltbyte = Encoding.ASCII.GetBytes(Salt);
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public IEnumerable<User> GetUser()
+        {
+            return _context.User;
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _context.User.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser([FromRoute] int id, [FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.UserID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<IActionResult> PostUser([FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: user.Password,
+            salt: Saltbyte,
+            prf: KeyDerivationPrf.HMACSHA1,
+            iterationCount: 10000,
+            numBytesRequested: 256 / 8));
+
+            user.Password = hashed;
+
+            _context.User.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.UserID }, user);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.User.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.User.Any(e => e.UserID == id);
+        }
+    }
+}
---
+++ UserLoginAPI/UserLoginAPI/Data/UserLoginAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserLoginAPI.Models
+{
+    public class UserLoginAPIContext : DbContext
+    {
+        public UserLoginAPIContext (DbContextOptions<UserLoginAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserLoginAPI.Models.User> User { get; set; }
+    }
+}
