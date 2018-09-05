3ad2fe4bcd5f7e960385d6ebeeda1ff9c3bec368
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-09-05 10:02:29 +0530

    Api has been updated, quizhub is created

---
+++ QuizEngineBackend/QuizServer/Models/ConnectionMapping.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizServer.Models
+{
+    public class ConnectionMapping
+    {
+        private readonly Dictionary<int , HashSet<string>> _connections =
+    new Dictionary<int, HashSet<string>>();
+
+        public int Count
+        {
+            get
+            {
+                return _connections.Count;
+            }
+        }
+
+        public void Add(int key, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if (!_connections.TryGetValue(key, out connections))
+                {
+                    connections = new HashSet<string>();
+                    _connections.Add(key, connections);
+                }
+
+                lock (connections)
+                {
+                    connections.Add(connectionId);
+                }
+            }
+        }
+
+        public IEnumerable<string> GetConnections(int key)
+        {
+            HashSet<string> connections;
+            if (_connections.TryGetValue(key, out connections))
+            {
+                return connections;
+            }
+
+            return Enumerable.Empty<string>();
+        }
+
+        public void Remove(int key, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if (!_connections.TryGetValue(key, out connections))
+                {
+                    return;
+                }
+
+                lock (connections)
+                {
+                    connections.Remove(connectionId);
+
+                    if (connections.Count == 0)
+                    {
+                        _connections.Remove(key);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ QuizEngineBackend/QuizServer/QuestionHub.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using QuizServer.Models;
+using QuizServer.C
+using Microsoft.AspNet.SignalR;
+namespace QuizServer
+{
+    public class QuestionHub : Hub
+    {
+        private readonly static ConnectionMapping<string> _connections =
+            new ConnectionMapping<string>();
+
+
+        //public  Dictionary<int, Dictionary<string, List<string>>> dict = new Dictionary<int, Dictionary<string, List<string>>>();
+        //Api call
+        //get the list of quetions for a particular domain
+        public static int count = 0;
+        List<Question> Questions = new List<Question>()
+        {
+            new Question() {
+            QuestionId = 1,
+            QuestionText = "Who is the CM of WestBengal?",
+            Options = new List<string>()
+                {
+                        "Siddu",
+
+                         "Didi",
+
+                    
+                        "Modi",
+
+                         "RaGa"
+                }
+         },
+                
+        
+        new Question()
+        {
+            QuestionId = 2,
+            QuestionText = "Who is the CM of Karnataka?",
+                      Options = new List<string>()
+                {
+                        "deepu",
+
+                         "shashaidar",
+
+                    
+                        "Deepika",
+
+                         "deepthi"
+                }
+         },
+            
+           new Question()
+        {
+            QuestionId = 3,
+            QuestionText = "Who is the CM of Kerala?",
+            Options = new List<string>()
+                {
+                        "Siddu",
+
+                         "Didi",
+
+                    
+                        "Modi",
+
+                         "RaGa"
+                }
+         },
+        new Question()
+        {
+            QuestionId = 4,
+            QuestionText = "Who is the best ----- developer in stack route?",
+            Options = new List<string>()
+                {
+                        "deepu",
+
+                         "deepu",
+
+                    
+                        "deepu",
+
+                         "FE"
+                }
+        }
+         
+        };
+
+        // public Task StoreResponse()
+        // {
+
+        // }
+
+        // Emit's Next Question from the collection of Questions
+        public Task GetNextQuestion()
+        {
+            if (count < Questions.Count) 
+            {
+                Question question = Questions[count];
+                QuestionAttempted q = new QuestionAttempted();
+                //q.difficultyLevel = 
+                count++;
+                // string q = question.QuestionText;
+                return Clients.Caller.SendAsync("NextQuestion", question);
+            }
+            else
+                return Clients.Caller.SendAsync("EndOfQuestions");
+        }
+
+
+        public override Task OnConnectedAsync()
+        {
+
+            Console.WriteLine("Client Connected");
+            Console.WriteLine("count before is " + count);
+            Console.WriteLine("count is " + count);
+
+            //return GetNextQuestion(user1);
+            return GetNextQuestion();
+
+        }
+
+        public Task send(string response, string userId)
+        {
+            Console.WriteLine(response);
+            return Clients.Caller.SendAsync("SendResponse", response);
+
+        }
+
+    }
+}
