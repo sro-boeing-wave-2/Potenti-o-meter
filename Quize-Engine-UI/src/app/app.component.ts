12f953dd714b0c9e9f7d61f11f4eb7b8e114f452
Deepika Mohan <deepikamm12@gmail.com>
2018-09-05 10:42:31 +0530

    player and mcq component working

---
+++ Quize-Engine-UI/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AdComponents } from './adComponent';
+import { PlayerService } from './player.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Quize-Engine-UI';
+  questionComponents: AdComponents[];
+  userId: string;
+  domain: string;
+
+  constructor(private playerService: PlayerService) {}
+
+  ngOnInit() {
+    this.questionComponents = this.playerService.getComponents();
+    this.userId = "deepikamohan";
+    this.domain = "java";
+  }
+}
---
+++ Quize-Engine-UI/src/app/questionModule.ts
@@ -0,0 +1,8 @@
+export class QuestionModel {
+  public Quesid : number
+  public QuestionText : string
+  public Options : string[]
+}
+
+
+
